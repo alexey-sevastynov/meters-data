9a21641deb7414f0cb89a4fcac25fff24eede44a
Alexey Sevastynov <alexseva94@gmail.com>
2024-01-04 20:33:22 +0200

    added page content Antonovicha73

--- src/pages/Antonovicha73.tsx
+++ src/pages/Antonovicha73.tsx
@@ -1,12 +1,17 @@
 import React, { HTMLAttributes } from "react";
 import "../styles/pages/antonovicha73.scss";
+import { InfoPanelMonth } from "../components/InfoPanelMonth/InfoPanelMonth";
+import { MetersData } from "../components/MetersData/MetersData";
 
 interface Antonovicha73Props extends HTMLAttributes<HTMLDivElement> {}
 
-export const Antonovicha73: React.FC<Antonovicha73Props> = ({ ...props }) => {
+export const Antonovicha73: React.FC<Antonovicha73Props> = () => {
   return (
-    <div className="antonovicha73" {...props}>
-      Antonovicha73
-    </div>
+    <section className="antonovicha73">
+      <h3 className="title">Volodymyr Antonovicha street, 73/8</h3>
+      <InfoPanelMonth isWaterBlock={false} />
+
+      <MetersData isWaterBlock={false} />
+    </section>
   );
 };
