64ef80d980f41555c5752786a1838ab1ab092680
Alexey Sevastynov <alexseva94@gmail.com>
2024-01-04 20:29:51 +0200

    added page content Antonovicha75

--- src/pages/Antonovicha75.tsx
+++ src/pages/Antonovicha75.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "../styles/pages/antonovicha75.scss";
+import { InfoPanelMonth } from "../components/InfoPanelMonth/InfoPanelMonth";
+import { MetersData } from "../components/MetersData/MetersData";
 
 export const Antonovicha75 = () => {
-  return <section className="antonovicha75">Antonovicha75</section>;
+  return (
+    <section className="antonovicha75">
+      <h3 className="title">Volodymyr Antonovicha street, 75/1</h3>
+      <InfoPanelMonth isWaterBlock={false} />
+
+      <MetersData isWaterBlock={false} />
+    </section>
+  );
 };
