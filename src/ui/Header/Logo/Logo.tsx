ecbcf5b4baa03fa074847f466b0e060b5a9a1dcc
Alexey Sevastynov <alexseva94@gmail.com>
2024-01-13 12:03:35 +0200

    Demo: added a temporary request to Logo to get all meters data

--- src/ui/Header/Logo/Logo.tsx
+++ src/ui/Header/Logo/Logo.tsx
@@ -1,9 +1,12 @@
 import Styles from "./logo.module.scss";
 import { getIconUrl } from "../../../helpers/getIconUrl";
+import { useDispatch } from "react-redux";
+import { fetchAllMetersData } from "../../../redux/slices/MetersDataSlice";
 
 export const Logo = () => {
+  const dispatch = useDispatch();
   return (
-    <div className={Styles.logo}>
+    <div className={Styles.logo} onClick={() => dispatch(fetchAllMetersData())}>
       <img src={getIconUrl("logo.png")} alt="logo" width={50} height={50} />
       <h3 className={Styles.text}>MetersData</h3>
     </div>
