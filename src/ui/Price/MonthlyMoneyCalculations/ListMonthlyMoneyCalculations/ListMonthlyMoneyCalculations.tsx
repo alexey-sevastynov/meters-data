05130a350456c42a12386a272600b3f70851f094
Alexey Sevastynov <alexseva94@gmail.com>
2024-01-15 23:28:32 +0200

    added the ability to search for monthly meter readings by date

--- src/ui/Price/MonthlyMoneyCalculations/ListMonthlyMoneyCalculations/ListMonthlyMoneyCalculations.tsx
+++ src/ui/Price/MonthlyMoneyCalculations/ListMonthlyMoneyCalculations/ListMonthlyMoneyCalculations.tsx
@@ -1,30 +1,20 @@
 import Style from "./listMonthlyMoneyCalculations.module.scss";
 import { ItemMonthlyMoneyCalculations } from "../ItemMonthlyMoneyCalculations/ItemMonthlyMoneyCalculations";
-import { useAppSelector } from "../../../../redux/hook";
-import { useLocation } from "react-router-dom";
-import { AddressType } from "../../../../types/MeterDataType";
+import { MonthlyMoneyCalculationsType } from "../../../../types/MonthlyMoneyCalculationsType";
 
-export const ListMonthlyMoneyCalculations = () => {
-  const { pathname } = useLocation();
+interface ListMonthlyMoneyCalculationsProps {
+  items: MonthlyMoneyCalculationsType[] | undefined;
+  status: string;
+}
 
-  const status = useAppSelector(
-    (props) => props.prices.itemsMonthlyMoneyCalculations.status
-  );
-  const allListItems = useAppSelector(
-    (props) => props.prices.itemsMonthlyMoneyCalculations.items
-  );
-
-  const currentPageName = pathname.replace(/^\/|\/price$/g, "") as AddressType;
-
-  const itemsFilter = allListItems?.filter(
-    (item) => item.address === currentPageName
-  );
-
+export const ListMonthlyMoneyCalculations: React.FC<
+  ListMonthlyMoneyCalculationsProps
+> = ({ items, status }) => {
   return (
     <ul className={Style.listMonthlyMoneyCalculations}>
       {status === "loading" && <p>loading...</p>}
-      {itemsFilter &&
-        itemsFilter.map(({ _id, data, sumMoney, address }) => (
+      {items &&
+        items.map(({ _id, data, sumMoney, address }) => (
           <ItemMonthlyMoneyCalculations
             key={_id}
             id={_id}
--- src/ui/Price/MonthlyMoneyCalculations/MonthlyMoneyCalculations.tsx
+++ src/ui/Price/MonthlyMoneyCalculations/MonthlyMoneyCalculations.tsx
@@ -2,13 +2,34 @@ import React, { useState } from "react";
 import Style from "./monthlyMoneyCalculations.module.scss";
 import { Input } from "../../../components/Input/Input";
 import { ListMonthlyMoneyCalculations } from "./ListMonthlyMoneyCalculations/ListMonthlyMoneyCalculations";
+import { useLocation } from "react-router-dom";
+import { useAppSelector } from "../../../redux/hook";
+import { AddressType } from "../../../types/MeterDataType";
 
 interface MonthlyMoneyCalculationsProps {}
 
 export const MonthlyMoneyCalculations: React.FC<
   MonthlyMoneyCalculationsProps
 > = () => {
+  const { pathname } = useLocation();
+
+  const status = useAppSelector(
+    (props) => props.prices.itemsMonthlyMoneyCalculations.status
+  );
+  const allListItems = useAppSelector(
+    (props) => props.prices.itemsMonthlyMoneyCalculations.items
+  );
+
   const [inputValue, setInputValue] = useState<string>("");
+
+  const currentPageName = pathname.replace(/^\/|\/price$/g, "") as AddressType;
+
+  const itemsFilter = allListItems?.filter(
+    (item) =>
+      item.address === currentPageName &&
+      item.data[0].description.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
   return (
     <section className={Style.monthlyMoneyCalculations}>
       <h4>Monthly money calculations:</h4>
@@ -25,7 +46,7 @@ export const MonthlyMoneyCalculations: React.FC<
         />
       </div>
 
-      <ListMonthlyMoneyCalculations />
+      <ListMonthlyMoneyCalculations items={itemsFilter} status={status} />
     </section>
   );
 };
