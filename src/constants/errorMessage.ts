b85ba348b385a1f96766abe285a91edd1caaba5f
alexseva94 <alexseva94@gmail.com>
2024-10-05 18:13:10 +0300

    5-calculate-total-day-and-night-meter-readings

---
+++ src/constants/errorMessage.ts
@@ -0,0 +1,4 @@
+export const errorMessage = {
+  invalidParameters:
+    "Invalid parameters: a = {0} ({1}), b = {2} ({3}). Expected numbers.",
+} as const;
---
+++ src/helpers/isNumber.ts
@@ -0,0 +1,3 @@
+export const isNumber = (value: unknown) => {
+  return typeof value === "number";
+};
