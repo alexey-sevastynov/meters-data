8aa47f01ebdde440a4da7e1dd9417aebdf353522
alexseva94 <alexseva94@gmail.com>
2024-10-26 19:56:29 +0300

    24-remove-headerlist-in-listmetersdata

--- src/ui/MetersData/ListMetersData/ListMetersData/ListMetersData.tsx
+++ src/ui/MetersData/ListMetersData/ListMetersData/ListMetersData.tsx
@@ -4,16 +4,9 @@ import GroupYear from "../GroupYear/GroupYear";
 import { useAppSelector } from "@/redux/hook";
 import { useLocation } from "react-router-dom";
 import useMetersData from "@/hooks/useMetersData";
-import {
-  HEIGHT_COMPONENT_HEADER,
-  WIDTH_COMPONENT_LIST_METERS_DATA_BIG,
-  WIDTH_COMPONENT_LIST_METERS_DATA_SMALL,
-} from "@/constants";
 import { GroupedData } from "@/types/MeterDataType";
 import { isEmptyObject } from "@/helpers/isEmptyObject";
-import { selectTranslations } from "@/redux/slices/I18next";
 import { groupAndSortItemsByYear } from "@/helpers/groupAndSortItemsByYear";
-import { checkScreenSize, handleScroll } from "./listMetersData.function";
 
 interface ListMetersDataProps {
   isWaterBlock: boolean;
@@ -22,8 +15,6 @@ interface ListMetersDataProps {
 export const ListMetersData: React.FC<ListMetersDataProps> = ({
   isWaterBlock,
 }) => {
-  const lang = useAppSelector(selectTranslations);
-
   const { isDataFromLocalStorage } = useMetersData();
   const { pathname } = useLocation();
 
@@ -32,8 +23,6 @@ export const ListMetersData: React.FC<ListMetersDataProps> = ({
   const addressCurrentPage = pathname.slice(1);
   const [groupedData, setGroupedData] = useState<GroupedData>({});
 
-  const [listMetersDataTop, setListMetersDataTop] = useState(0);
-  const [listMetersDataWidth, setListMetersDataWidth] = useState(0);
   const [idActiveBtn, setIdActiveBtn] = useState<string>("");
 
   const listMetersDataRef = useRef<HTMLUListElement>(null);
@@ -45,51 +34,13 @@ export const ListMetersData: React.FC<ListMetersDataProps> = ({
     }
   }, [items, addressCurrentPage]);
 
-  const handleScrollEvent = () => {
-    handleScroll(listMetersDataRef, setListMetersDataTop);
-  };
-  const checkScreenSizeEvent = () => {
-    checkScreenSize(listMetersDataRef, setListMetersDataWidth);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScrollEvent);
-    window.addEventListener("scroll", checkScreenSizeEvent);
-    window.addEventListener("resize", checkScreenSizeEvent);
-
-    return () => {
-      window.removeEventListener("scroll", handleScrollEvent);
-      window.removeEventListener("scroll", checkScreenSizeEvent);
-      window.removeEventListener("resize", checkScreenSizeEvent);
-    };
-  }, []);
-
   const isEmptyList = isEmptyObject(groupedData) && status === "loaded";
-  const widthComponent =
-    ((addressCurrentPage === "chelyuskina" ||
-      addressCurrentPage === "slobozhansky-68a") &&
-      WIDTH_COMPONENT_LIST_METERS_DATA_BIG) ||
-    WIDTH_COMPONENT_LIST_METERS_DATA_SMALL;
 
   return (
     <ul
       ref={listMetersDataRef}
       className={Style.listMetersData}
     >
-      {listMetersDataTop < HEIGHT_COMPONENT_HEADER &&
-        listMetersDataWidth > widthComponent && (
-          <li className={Style.headerList}>
-            <p>{lang.infoPanel["date"]}:</p>
-            <p>{lang.infoPanel["Light general"]}:</p>
-            <p>{lang.infoPanel["Light day"]}:</p>
-            <p>{lang.infoPanel["Light night"]}:</p>
-            <p>{lang.infoPanel["Gas General"]}:</p>
-            {(addressCurrentPage === "chelyuskina" ||
-              addressCurrentPage === "slobozhansky-68a") && (
-              <p>{lang.infoPanel["Water general"]}:</p>
-            )}
-          </li>
-        )}
       {status === "loading" && <li>Loading...</li>}
       {isEmptyList ? (
         <p>{isDataFromLocalStorage ? "No cached items" : "No items"}</p>
