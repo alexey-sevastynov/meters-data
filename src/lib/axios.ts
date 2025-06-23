23ff1d04cb009a61a71ad03044178769630de487
alexseva94 <alexseva94@gmail.com>
2025-06-23 14:09:22 +0300

    167-api-fix-missing-endpoint

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,8 +5,16 @@ import { envKeys } from "@/infra/env/env-keys";
 const apiKey = getStringEnv(envKeys.apiKey);
 
 export const apiClient = axios.create({
-    baseURL: getStringEnv(envKeys.apiUrl),
+    baseURL: getApiBaseUrl(),
     headers: {
         "x-api-key": apiKey,
     },
 });
+
+function getApiBaseUrl() {
+    const apiUrl = getStringEnv(envKeys.apiUrl);
+    const apiPrefix = "/api";
+    const apiBaseUrl = apiUrl + apiPrefix;
+
+    return apiBaseUrl;
+}
