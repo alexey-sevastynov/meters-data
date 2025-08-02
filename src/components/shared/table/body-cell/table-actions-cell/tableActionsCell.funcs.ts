c33aead768fefe345525f6b3a76d9f9ed5d2117b
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2025-08-02 22:54:27 +0300

    171 - status-update (#172)

--- src/components/shared/table/body-cell/table-actions-cell/tableActionsCell.funcs.ts
+++ src/components/shared/table/body-cell/table-actions-cell/tableActionsCell.funcs.ts
@@ -1,3 +1,5 @@
+import { emitMetaDataSocketEvent } from "@/infra/socket/socket-client";
+import { socketEventNames } from "@/infra/socket/socket-event-names";
 import { confirmActionExit, confirmActionOnDelete, setIdDelete } from "@/store/slices/confirm-popup-slice";
 import { deleteMeterData, getAllMetersData } from "@/store/slices/meters-data/meters-data.thunks";
 import { AppDispatch } from "@/store/store";
@@ -5,6 +7,8 @@ import { AppDispatch } from "@/store/store";
 export function deleteItemMeterData(id: string, dispatch: AppDispatch) {
     dispatch(deleteMeterData({ id })).then((response) => {
         if (response.payload) dispatch(getAllMetersData());
+
+        emitMetaDataSocketEvent(socketEventNames.deleteItem, response.payload);
     });
 
     dispatch(confirmActionOnDelete(false));
---
+++ src/infra/socket/socket-client.ts
@@ -0,0 +1,37 @@
+import { isDev } from "@/lib/environments";
+import { io, Socket } from "socket.io-client";
+import { AppDispatch } from "@/store/store";
+import { getAllMetersData } from "@/store/slices/meters-data/meters-data.thunks";
+import { SocketEventName, socketEventNames } from "@/infra/socket/socket-event-names";
+
+const socketUrl = isDev() ? "http://localhost:3000" : "https://meters-data-backend.vercel.app";
+
+let socket: Socket | null = null;
+
+export function initSocket(dispatch: AppDispatch) {
+    if (!socket) {
+        socket = io(socketUrl, { transports: ["websocket"] });
+
+        socket.on("connect", () => {
+            console.log("Socket connected:", socket?.id);
+        });
+
+        socket.on(socketEventNames.createItem, () => {
+            dispatch(getAllMetersData());
+        });
+
+        socket.on(socketEventNames.updateItem, () => {
+            dispatch(getAllMetersData());
+        });
+
+        socket.on(socketEventNames.deleteItem, () => {
+            dispatch(getAllMetersData());
+        });
+    }
+}
+
+export function emitMetaDataSocketEvent(eventName: SocketEventName, payload: unknown) {
+    if (socket) {
+        socket.emit(eventName, payload);
+    }
+}
